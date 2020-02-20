647a9dc8722d8fb955059f3fcce35415d5e58a41
Modi <umodi@albany.edu>
2020-02-20 18:47:14 -0500

    Adding accordion changes

---
+++ js/accordion.js
@@ -0,0 +1,20 @@
+let accordions = document.getElementsByClassName('accordion-header');
+
+let hideshowfunction = function(){  
+	if(this.nextElementSibling.style.display != "block")
+	{
+        for (let i = 0; i < accordions.length; i++) {
+            accordions[i].nextElementSibling.style.display = "none";      
+        }
+        this.nextElementSibling.style.display = "block";
+	}
+	
+else if(this.nextElementSibling.style.display == "block")
+	{
+         this.nextElementSibling.style.display = "none";
+    }
+}
+
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', hideshowfunction, false);
+}
