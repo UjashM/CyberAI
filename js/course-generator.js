79315468f55521118d73e43204b9f47d435e2342
Modi <umodi@albany.edu>
2020-02-21 18:55:26 -0500

    Updated course offering pages as per IE browser

--- js/course-generator.js
+++ js/course-generator.js
@@ -1,21 +1,71 @@
-const coursetemplate = document.getElementById('course-template');
-
-let requestURL = "data/courses.json"
+let requestURL = "data/courses.json";
 let request = new XMLHttpRequest();
+let maincontentContainer = document.getElementsByClassName('main-content')[0];
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function(){
-    const courses = request.response;
-    courses.forEach(course => {
-        const instance = document.importNode(coursetemplate.content, true);
-        instance.querySelector('.course-code').innerHTML = course[0] + ' '+ course[1];
-        instance.querySelector('.course-title').innerHTML = course[2];
-        instance.querySelector('.description').innerHTML = course[4]; 
-        instance.querySelector('.department').innerHTML = course[6];
-        instance.querySelector('.school').innerHTML = course[5];
-        instance.querySelector('.credits').innerHTML = '('+ course[3]+ ' credits)'; 
-        instance.querySelector('.degree').innerHTML = course[7];
-        document.getElementsByClassName('main-content')[0].appendChild(instance);
-    })
+const courses = request.response;  
+    
+if(false || !!document.documentMode)
+{
+    let coursearray = JSON.parse(courses);
+    console.log(coursearray);
+    for(let i = 0; i < courses.length; i++)
+    {
+            //code for IE
+            let courseContainerElement = document.createElement("div");
+            courseContainerElement.classList.add('course-container');
+            let courseCodeElement = document.createElement("span");
+            courseCodeElement.appendChild(document.createTextNode(coursearray[i][0]));
+            courseCodeElement.appendChild(document.createTextNode(' '));
+            courseCodeElement.appendChild(document.createTextNode(coursearray[i][1]));
+            courseCodeElement.appendChild(document.createTextNode(' '));
+            courseCodeElement.appendChild(document.createTextNode(coursearray[i][2]));
+            courseCodeElement.appendChild(document.createTextNode(' ('));
+            courseCodeElement.appendChild(document.createTextNode(coursearray[i][3]));
+            courseCodeElement.appendChild(document.createTextNode(' credits)'));
+            courseContainerElement.appendChild(courseCodeElement); 
+            courseContainerElement.appendChild(document.createElement("br"));
+            departmentElement = document.createElement("span");
+            departmentElement.appendChild(document.createTextNode('Department: '));
+            departmentElement.appendChild(document.createTextNode(coursearray[i][6]));
+            courseContainerElement.appendChild(departmentElement);
+            courseContainerElement.appendChild(document.createElement("br"));
+            degreeElement = document.createElement("span");
+            degreeElement.appendChild(document.createTextNode('Degree: '));
+            degreeElement.appendChild(document.createTextNode(coursearray[i][7]));
+            courseContainerElement.appendChild(degreeElement);
+            courseContainerElement.appendChild(document.createElement("br"));
+            descriptionElement = document.createElement("p");
+            descriptionElement = document.createTextNode(coursearray[i][4]);
+            courseContainerElement.appendChild(descriptionElement);
+            maincontentContainer.appendChild(courseContainerElement);
+    }
+            
 }
+        else
+        {   
+            for(let i = 0; i < courses.length; i++)
+            {
+            let courseCode =  courses[i][0] + ' '+ courses[i][1];
+            //console.log(courseCode);
+            let courseTitle = courses[i][2];
+            let courseCredits = '('+ courses[i][3]+ ' credits)';
+            let description = courses[i][4];
+            let department = courses[i][6];
+            let school = courses[i][5];
+            let degree = courses[i][7];     
+            let coursetemplate =    
+                    '<strong><span class = "course-code">' + courseCode + '</span> &nbsp; &nbsp;<span class = "course-title">' + courseTitle+ '</span>'+
+                    '<span class = "credits">'+ courseCredits + '</span></strong>' +
+                    '<br><span><strong>Department: </strong></span><span class = "department">' + department + '</span><br><span><strong>School: </strong></span><span class = "school">'
+                    + school + '</span><br><span><strong>Degree: </strong></span><span class = "degree">' + degree+ '</span><br><p class= "description">' + description
+                    +'</p>';
+            let courseElement = document.createElement('div');
+            courseElement.classList.add('course-container');
+            courseElement.innerHTML = coursetemplate.trim();
+            maincontentContainer.appendChild(courseElement); 
+            }
+        }
+    }
